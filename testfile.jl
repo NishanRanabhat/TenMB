9578c19042b85d671cfafaced69a4a7c6ede3cfb
Nishan Ranabhat <78109661+NishanRanabhat@users.noreply.github.com>
2022-11-07 23:03:05 +0100

    Update testfile.jl

--- testfile.jl
+++ testfile.jl
@@ -1,15 +1,17 @@
 """
-This is a file that tests our codes, we call the main module as using .TenMB .
+This is a file that tests our codes. We will show how to run a DMRG and TDVP code with pure states for transverse field Ising Hamiltonian.
 """
 
 using .TenMB
 
 
 #building Ising hamiltonian
+
+#parameters
 h = 0.5; #transverse field
 N = 20; #system size
 spins = "Pauli"; #Pauli spins
-longdir = "X"; #longitudinar direction along X axis
+longdir = "X"; #longitudinal direction along X axis
 
 Ham = TenMB.Hamiltonian_Ising(h,N,spins,longdir); #Ham is a MPO
 
